92246ee0027472664323df63ad2ea80f299ba9c8
Ethan.Walters <Ethan.Walters@mu-c1-03.mullen.pvt.k12.co.us>
2017-08-31 15:10:07 -0600

    more work

--- Modulus Quizzer Project/src/ModQuiz.java
+++ Modulus Quizzer Project/src/ModQuiz.java
@@ -14,22 +14,20 @@ public class ModQuiz
 				
 				int numberOfQuestions = numberOfQuestionsScanner.nextInt();
 				
-				System.out.println(numberOfQuestions);
 				
 				for(int i = numberOfQuestions ; i > 0 ; i--){
 				
-					switch(numberOfQuestions){
+					switch(i){
 						case 1:
 							{
 								Random rad = new Random();
 								
 								int randomNumber = rad.nextInt(10) + 1;
-								int randomNumberOne = rad.nextInt(10) + 1;
 								
 								
-								int firstSolution = randomNumber % randomNumberOne;
+								int firstSolution = randomNumber % randomNumber;
 								
-								System.out.println(randomNumber + " % " + randomNumberOne + " = ?" );
+								System.out.println(randomNumber + " % " + randomNumber + " = ?" );
 								
 								Scanner solutionOne = new Scanner(System.in);
 								
@@ -45,19 +43,43 @@ public class ModQuiz
 								}
 								
 								}
+							break;
+						case 2:
+								{
+									Random rad = new Random();
+									
+									int randomNumber = rad.nextInt(10) + 20;
+									int randomNumberOne = rad.nextInt(10) + 10;
+									
+									
+									int firstSolution = randomNumberOne % randomNumber;
+									
+									System.out.println(randomNumberOne + " % " + randomNumber + " = ?" );
+									
+									Scanner solutionOne = new Scanner(System.in);
+									
+									int solutionOneInt = solutionOne.nextInt();
+									
+									if ( firstSolution == solutionOneInt){
+										System.out.println("Youre right!");
+										numberRight = numberRight + 1;
+									}
+									else{
+										System.out.println("Youre wrong!");
+										numberWrong = numberWrong +1;
+									}
+									
+									}
 							}
-					break;
+
 					
-				}
+				
 				
 					Random radOne = new Random();
 				
-				int randomNumber = radOne.nextInt(10) + 1;
+				int randomNumber = radOne.nextInt(10) + 10;
 				int randomNumberOne = radOne.nextInt(10) + 1;
 				
-				if ( randomNumber > randomNumberOne){
-					numberOfQuestions = numberOfQuestions + 1;
-				}
 				
 				int firstSolution = randomNumber % randomNumberOne;
 				
